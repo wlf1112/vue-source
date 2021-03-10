888dc931de668237d84ece9eb01881f66c762ebc
王丽峰 <wanglifeng@wanglifengdeMacBook-Pro.local>
2021-03-10 22:43:53 +0800

    init

---
+++ src/core/instance/index.js
@@ -0,0 +1,25 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+// 构造函数
+function Vue (options) {
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  // 初始化
+  this._init(options)
+}
+
+initMixin(Vue) //通过该方法给Vue添加_init方法
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
