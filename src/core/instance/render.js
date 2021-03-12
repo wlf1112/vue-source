e0f55d1e12d50a08660ba21a73bb42ebcfd95c97
WangLifeng <2322825963@qq.com>
2021-03-12 15:12:38 +0800

    Update render.js

--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -28,10 +28,11 @@ export function initRender (vm: Component) {
   // so that we get proper render context inside it.
   // args order: tag, data, children, normalizationType, alwaysNormalize
   // internal version is used by render functions compiled from templates
+  // 模板编译成的render函数使用
   vm._c = (a, b, c, d) => createElement(vm, a, b, c, d, false)
   // normalization is always applied for the public version, used in
   // user-written render functions.
-  // render(h)此处的$createElement就是h
+  // render(h)此处的$createElement就是h，用户手写render方法使用
   vm.$createElement = (a, b, c, d) => createElement(vm, a, b, c, d, true)
 
   // $attrs & $listeners are exposed for easier HOC creation.
